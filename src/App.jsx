0feabb919072d6d8bdb9335b46116bf2421402a2
Aman Kumar <authorisedaman@gmail.com>
2024-08-04 23:54:35 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,6 @@ const App = () => {
     "#06038D",
   ]);
   const colorGenerator = () => {
-    setClick(null);
-    setDisplay(null);
     const letters = "0123456789ABCDEF";
     let color1 = "#";
     for (let i = 0; i < 6; i++) {
@@ -32,27 +30,35 @@ const App = () => {
     setColors([color1, color2, color3, color4]);
   };
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    const subscribe = setInterval(() => {
+      colorGenerator();
+    }, 500);
+
+    return () => {
+      clearInterval(subscribe);
+    };
+  }, []);
 
   return (
     <div className="flex justify-center items-center flex-col md:grid md:grid-cols-2 md:justify-between pt-10 select-none min-h-[90vh]">
-      <div className="overflow-hidden  relative  ">
-        <img
+      <div className="overflow-hidden  relative flex justify-center items-center  ">
+        {/* <img
           src="./mac.png"
           alt=""
           className="h-[20vh] md:h-[40vh] md:ml-20  overflow-hidden z-[999]"
-        />
-        {/* <div className=" grid grid-cols-4 w-[70%]  absolute left-9 z-0 ">
+        /> */}
+        <div className=" grid grid-cols-4 w-[70%] z-0   overflow-x-hidden rounded  ">
           {colors &&
             colors?.map((c, i) => (
               <div
                 key={i}
-                className={`bg-[${c}] h-[15vh] w-[12vh] mt-[-143px]`}
+                className={`bg-[${c}] h-[20vh] w-[12vh] sm:h-[35vh] sm:w-[20vh] md:h-[35vh] md:w-[20vh] `}
               ></div>
             ))}
-        </div> */}
+        </div>
       </div>
-      <div className="mx-10 mt-5 text-center grid grid-cols-1 md:text-left">
+      <div className="mx-10 mt-5 text-center flex flex-col justify-center items-center md:items-start md:text-left">
         <h1 className="font-bold text-4xl md:text-5xl">
           The Super fast color palettes generator!
         </h1>
@@ -64,9 +70,11 @@ const App = () => {
         </p>
         <Link
           to={"/generate"}
-          className="bg-gradient-to-r from-[#4135a8]  via-[#2e1fd2] to-[#26268a]  text-white rounded-lg p-3   text-center w-[280px]  font-medium mt-4"
+          className="text-white  w-full  text-center font-medium  flex justify-center items-center md:justify-start"
         >
-          Start the generator
+          <div className="mt-4 w-[80%] md:w-[60%] text-center  border p-2 bg-gradient-to-r from-[#4135a8]  via-[#2e1fd2] to-[#26268a] rounded">
+            Start the generator
+          </div>
         </Link>
       </div>
     </div>
